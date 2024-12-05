6215a105532681ae34eae13e4d9d13d9fd662261
Margeo2 <102567068+Margeo2@users.noreply.github.com>
2024-12-05 17:09:46 -0300

    Subir todos los cambios

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 import streamlit as st
 from supabase_connection import connect_supabase, load_parquet_from_supabase, get_file_update_date
-from data_processing import group_by_program_type, filter_by_date
+from data_processing import group_by_program_type, group_by_desembolso, filter_by_date, process_data
 from file_export import download_excel
 import pandas as pd
-from module_css import apply_custom_css
+from module_css import apply_custom_css, csstitulo, csstabla, csstablafiltro
+import data_processing
 
 # Llamamos a la función que aplica el CSS
 apply_custom_css()
@@ -16,18 +17,71 @@ bucket_name = "INFO"
 file_name = "VT_NOMINA_REP_RECUPERO_X_ANIO_MVD.parquet"
 file_name_calendario = "VT_RECUPERO_CALENDARIO.parquet"
 
+# Aplicar el CSS al markdown
+st.markdown(f'<style>{csstitulo}</style>', unsafe_allow_html=True)
+
 # Obtener fecha de actualización
 update_date = get_file_update_date(client, bucket_name, file_name)
-st.markdown(f"**INFORME** \n\n MAS VIDA DIGNA - LO TENGO - SEMILLA  \n\n Actualizado al **{update_date}**")
+st.markdown(
+    f"""
+    <div class="custom-header">
+        INFORME  <br>
+        MAS VIDA DIGNA - LO TENGO - SEMILLA <br>
+        <span class="updated-text">Actualizado al</span>
+        <b class="date-text">{update_date}</b>
+    </div>
+    """,
+    unsafe_allow_html=True,
+)
+
+# Crear un contenedor para las tablas (usando el CSS definido)
+st.markdown('<div class="table-container">', unsafe_allow_html=True)
+
+# Aplicar el CSS al markdown
+st.markdown(f'<style>{csstabla}</style>', unsafe_allow_html=True)
 
 # Cargar datos de VT_NOMINA_REP_RECUPERO_X_ANIO_MVD
 df = load_parquet_from_supabase(client, bucket_name, file_name)
+
 if df is not None:
     # Agrupar datos
     df_grouped = group_by_program_type(df)
-    # Mostrar el cuadro agrupado con nombre personalizado
-    st.markdown('<p class="df_grouped_header">Resumen por TIPO_PROGRAMA</p>', unsafe_allow_html=True)
-    st.dataframe(df_grouped)
+    
+    # Filtrado ID_DESEMBOLSO x MAS VIDA DIGNA
+    try:
+        df_procesado = process_data(df, filtro_tipo_programa="MVD")
+
+        # Mostrar las tablas una al lado de la otra usando el contenedor
+        st.markdown(
+            """
+            <div class="table-container">
+                <div class="table">
+                    <p class="df_grouped_header">Resumen - Programas</p>
+                    {0}
+                </div>
+                <div class="table">
+                    <p class="df_grouped_header">Resumen - Desembolsos Mas Vida Digna</p>
+                    {1}
+                </div>
+            </div>
+            """.format(df_grouped.to_html(index=False), df_procesado.to_html(index=False)),
+            unsafe_allow_html=True
+        )
+        
+    except ValueError as e:
+        # Manejar errores en caso de que las columnas necesarias no estén presentes
+        st.error(f"Error al procesar los datos: {str(e)}")
+else:
+    st.error("No se pudo cargar el archivo desde Supabase.")      
+
+# Cerrar el contenedor
+st.markdown('</div>', unsafe_allow_html=True)        
+
+# Crear un contenedor para las tablas (usando el CSS definido)
+st.markdown('<div class="table-container">', unsafe_allow_html=True)
+
+# Aplicar el CSS al markdown
+st.markdown(f'<style>{csstablafiltro}</style>', unsafe_allow_html=True)
 
 # Cargar y filtrar datos de VT_RECUPERO_CALENDARIO
 df_calendario = load_parquet_from_supabase(client, bucket_name, file_name_calendario)
@@ -40,18 +94,10 @@ if df_calendario is not None:
         df_calendario['MES'] = df_calendario['PERIODO_CUOTA'].dt.month
         df_calendario['AÑO'] = df_calendario['PERIODO_CUOTA'].dt.year
         
-        #df_calendario['AÑO'] = df_calendario['AÑO'].astype(str).str.replace(',', '', regex=False)#.astype(int)
-
-        # Verificar que la conversión se hizo correctamente
-        st.write(f"Tipo de dato de la columna 'AÑO' después de la conversión: {df_calendario['AÑO'].dtype}")
-
         # Reordenar las columnas, colocando 'AÑO' y 'MES' al principio
         cols = ['AÑO', 'MES'] + [col for col in df_calendario.columns if col not in ['AÑO', 'MES']]
         df_calendario = df_calendario[cols]
         
-        # Eliminar la columna 'PERIODO_CUOTA'
-        #df_calendario = df_calendario.drop(columns=['PERIODO_CUOTA'])
-
         # Filtros por mes y año en Streamlit
         meses_disponibles = sorted(df_calendario['MES'].unique())
         anios_disponibles = sorted(df_calendario['AÑO'].unique())
@@ -69,10 +115,28 @@ if df_calendario is not None:
 
         # Filtrar por fechas
         df_filtrado = filter_by_date(df_calendario, fecha_inicio, fecha_fin)
+
+        # Agregar saltos de línea
+        st.write("\n\n\n\n\n\n\n")  # Esto agrega saltos de línea
+
+        # Hacer el texto en negrita y cambiar el tamaño usando HTML
+        st.markdown('<p style="font-size: 18px; font-weight: bold;">RECUPERO:</p>', unsafe_allow_html=True)
+
         st.write(f"Datos filtrados desde {fecha_inicio.strftime('%d-%m-%Y')} hasta {fecha_fin.strftime('%d-%m-%Y')}:")
+        
+        # Formateo campo AÑO
+        df_filtrado['AÑO'] = df_filtrado['AÑO'].astype(str)
+
+        # Eliminar la columna 'PERIODO_CUOTA'
+        df_filtrado = df_filtrado.drop(columns=['PERIODO_CUOTA'])
         st.dataframe(df_filtrado)
 
         # Botón para descargar como Excel
         download_excel(df_filtrado, fecha_inicio, fecha_fin)
     else:
         st.error("La columna 'PERIODO_CUOTA' no existe en el archivo.")
+
+# Cerrar el contenedor
+st.markdown('</div>', unsafe_allow_html=True)              
+
+print(dir(data_processing))  
--- module_css.py
+++ module_css.py
@@ -1,5 +1,26 @@
 import streamlit as st
 
+# Estilos CSS para el encabezado y la visualización de las tablas
+csstitulo = """
+.custom-header {
+    font-family: Arial, sans-serif;
+    font-size: 20px;
+    font-weight: bold;
+    text-align: center;
+    color: #333;
+}
+
+.custom-header .updated-text {
+    font-size: 14px;
+    color: #FF5733;
+}
+
+.custom-header .date-text {
+    font-size: 18px;
+    color: #2C3E50;
+}
+"""
+
 def apply_custom_css():
     """
     Aplica los estilos CSS personalizados para ajustar la visualización de las tablas y el cuadro de texto.
@@ -15,22 +36,48 @@ def apply_custom_css():
 
         /* Modificar tamaño de la cabecera de la grilla */
         .dataframe thead th {
-            font-size: 16px !important;
+            font-size: 14px !important;
+            text-align: center; /* Centrado */    
         }
 
-        /* Estilo personalizado para el nombre del cuadro df_grouped */
+        /* Estilo personalizado para el Titulo del cuadro df_grouped (Desembolsos Mas Vida Digna) */
         .df_grouped_header {
             font-size: 18px; /* Tamaño de la fuente para el cuadro df_grouped */
-            color: #4CAF50;  /* Color verde para el nombre */
+            color: #333;  /* Color para el nombre */
             font-weight: bold; /* Negrita */
             text-align: center; /* Centrado */
         }
-
-        /* Estilo para la tabla filtrada (df_filtrado) */
-        .stDataFrame {
-            width: 100% !important;
-            margin: auto;
-            padding: 10px;
-        }
     </style>
     """, unsafe_allow_html=True)
+
+
+# CSS para organizar las tablas una al lado de la otra
+csstabla = """
+.table-container {
+    display: flex;
+    justify-content: space-between;
+    gap: 20px;  /* Espacio entre las tablas */
+    width: 100%;  /* Hace que el contenedor ocupe todo el ancho disponible */
+}
+
+.table-container .table {
+    width: 48%;  /* Ajuste del tamaño de las tablas */
+}
+"""
+
+# CSS para asegurar que las tablas ocupen todo el ancho disponible
+csstablafiltro = """
+.table-container {
+    width: 100%;  /* Hace que el contenedor ocupe todo el ancho disponible */
+    overflow-x: auto;  /* Permite desplazamiento horizontal si la tabla es muy ancha */
+}
+
+.stDataFrame > div {
+    width: 100% !important;  /* Asegura que el ancho de la tabla sea 100% */
+}
+
+.stDataFrame table {
+    width: 100% !important;  /* Hace que la tabla ocupe todo el ancho */
+    table-layout: fixed;  /* Hace que las celdas se distribuyan uniformemente */
+}
+"""
